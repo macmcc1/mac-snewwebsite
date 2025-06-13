bdc89c891291bff17b03effef1cc4557c845f949
macmcc1 <mcslide47@gmail.com>
2025-06-13 21:05:21 +0000

    Added and deleted some text from web.js

--- assets/web.js
+++ assets/web.js
@@ -1,12 +1,6 @@
 document.querySelectorAll('a[target="_blank"]').forEach(link => {
   link.addEventListener('click', () => {
     console.log(`Opening ${link.href}`);
-  });
-});
-document.querySelectorAll('a[target="_blank"]').forEach(link => {
-  link.addEventListener('click', (event) => {
-    event.preventDefault(); // Prevent default action
-    console.log(`Opening ${link.href}`);
-    window.open(link.href, '_blank'); // Open in a new tab
+    // No need for preventDefault or window.open
   });
 });
