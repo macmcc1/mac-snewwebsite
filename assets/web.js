59fcc7d13e0542d767b1e971b6e8f6d519811e6e
macmcc1 <mcslide47@gmail.com>
2025-06-13 21:00:21 +0000

    erased everything in the js file.

--- assets/web.js
+++
@@ -1,9 +0,0 @@
-// script.js
-document.getElementById('placeholderInput').addEventListener('keydown', function(e) {
-  if (e.key === 'Enter') {
-    const input = e.target.value;
-    if (input) {
-      alert(`You entered: ${input}`);
-    }
-  }
-});
